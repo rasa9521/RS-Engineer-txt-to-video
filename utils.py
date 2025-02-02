7a1932fa68740b62170804d8198a4dfb8bf62e41
indianup <jaibajarangclasses@gmail.com>
2025-02-02 18:47:26 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,4 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
+# Don't Remove Credit Telegram - @Engineers_Babu
 import time
 import math
 import os
