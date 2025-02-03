1bb665c1ec117ae29c1fe00449fca1707b4052cc
ミ★ 𝕰𝖓𝖌𝖎𝖓𝖊𝖊𝖗𝖘 𝕭𝖆𝖇𝖚 ★彡 <jaibajarangclasses@gmail.com>
2025-02-03 21:49:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -68,14 +68,12 @@ image_urls = [
     "https://i.ibb.co/rQMXQjX/file-3961.jpg",
 ]
 random_image_url = random.choice(image_urls)
-    caption = (
-        "**𝐇𝐞𝐥𝐥𝐨 𝐃𝐞𝐚𝐫 👋!**\n\n"
+    caption = ("**𝐇𝐞𝐥𝐥𝐨 𝐃𝐞𝐚𝐫 👋!**\n\n"
         "➠ **𝐈 𝐚𝐦 𝐚 𝐓𝐞𝐱𝐭 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐞𝐫 𝐁𝐨𝐭 𝐌𝐚𝐝𝐞 𝐖𝐢𝐭𝐡 ♥️**\n"
         "➠ **Can Extract Videos & PDFs From Your Text File and Upload to Telegram!**\n"
-        "➠ **For Guide Use Command /guide 📖**\n\n"
-        "➠ **Use /moni Command to Download From TXT File** 📄\n\n"
-        "➠ **𝐌𝐚𝐝𝐞 𝐁𝐲:** @SanjayKagra86🩷"
-    )
+        "➠ **For Guide Use Command /guide 📖**\n"
+        "➠ **Use /moni Command to Download From TXT File** 📄\n"
+        "➠ **𝐌𝐚𝐝𝐞 𝐁𝐲:** @SanjayKagra86🩷")
 
 # Start command handler
 @bot.on_message(filters.command(["start"]))
