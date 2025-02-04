69c099fadb3cba2b476a1de977bf7f1c5f55d419
ミ★ 𝕰𝖓𝖌𝖎𝖓𝖊𝖊𝖗𝖘 𝕭𝖆𝖇𝖚 ★彡 <jaibajarangclasses@gmail.com>
2025-02-04 15:24:32 +0530

    Update logs.py

--- logs.py
+++ logs.py
@@ -1,6 +1,4 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+# Don't Remove Credit Tg - @Engineers_Babu
 
 import logging
 from logging.handlers import RotatingFileHandler
