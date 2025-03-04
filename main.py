345753b7c2e72259486ec0c41ddfaa38161d5d07
ミ★ 𝕰𝖓𝖌𝖎𝖓𝖊𝖊𝖗𝖘 𝕭𝖆𝖇𝖚 ★彡 <jaibajarangclasses@gmail.com>
2025-03-05 01:52:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,11 @@ from pyrogram.types import Message
 from pyromod import listen
 
 from utils import get_datetime_str, create_html_file
+
+
+API_ID = int(environ.get("API_ID", "21705536"))
+API_HASH = environ.get("API_HASH", "c5bb241f6e3ecf33fe68a444e288de2d")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8013725761:AAGQyr32ibk7HQNqxv4FSD2ZrrSLOmzknlg")
 # ==========Logging==========#
 logging.basicConfig(
     level=logging.DEBUG,
